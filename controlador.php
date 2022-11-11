f8b179750c5ad90f24fac343bbacd3ac7fbb54c4
Luisj122 <luisjmnz122@gmail.com>
2022-11-11 17:45:15 +0100

    modelo

--- controlador.php
+++ controlador.php
@@ -19,10 +19,25 @@ if (isset($_GET['accion'])) {
         borrarJuego($_GET['id']);
             header("Location: index.php");
     }
-
     // Eliminar localizacion
     if ($_GET['accion'] == 'borrarL') {
         borrarLocalizacion($_GET['id']);
             header("Location: index.php");
     }
+    
 }
+
+// POST ----------------------------------------------------
+if ($_POST) {
+    // Comprobamos si se ha enviado algo por el formulario de juego nuevo
+    if (isset($_POST['nuevoJuego'])) {
+        $nombre = filtrado($_POST['nombre']);
+        $descripcion = filtrado($_POST['descripcion']);
+        $plataforma = filtrado($_POST['plataforma']);
+        $genero = filtrado($_POST['genero']);
+
+        insertarJuego($nombre, $descripcion, $plataforma, $genero);
+    }
+}
+
+?>
