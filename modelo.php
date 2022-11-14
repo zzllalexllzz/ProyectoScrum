85b044cb65421def39fe28b80786f7cadf253600
rzafr <rubenzafraramirez@gmail.com>
2022-11-14 21:45:31 +0100

    local

--- modelo.php
+++ modelo.php
@@ -1,6 +1,6 @@
 <?php
 
-    /*
+
     function conexionBD() {
         $dbh = null;
 
@@ -13,8 +13,8 @@
 
         return $dbh;
     }
-    */
-
+    
+    /*
     function conexionBD() {
         $dbh = null;
 
@@ -27,7 +27,7 @@
         }
 
         return $dbh;
-    }
+    }*/
     
     
     /**
